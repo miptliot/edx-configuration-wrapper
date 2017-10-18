6e6cd7d2f45e5625e39b7d02b6e8b8f7ceafedd2
Serge Pokhodyaev <snp@z9.ru>
2017-10-18 16:47:07 +0300

    add more symlinks during the bootstrap

--- do_bootstrap.sh
+++ do_bootstrap.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+function usage() {
+  echo "Usage: $0 PATH_TO_MAIN_PLAYBOOKS" >&2
+  exit 1
+}
+
 set -e
+
+[[ -n "$1" ]] || usage
 
 git submodule init
 git submodule update
@@ -10,4 +17,8 @@ venv/bin/pip install -r edx-configuration/requirements.txt
 
 for i in venv/bin/ansible*; do
   ln -s $i
+done
+
+for d in group_vars inventory lookup_plugins maps pw; do
+  ln -s $1/$d
 done
